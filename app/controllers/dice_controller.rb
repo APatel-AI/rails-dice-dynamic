cd378bba5d3c5cafadee5070b6192c015ab8e658
Arpan Patel <113603155+APatel-AI@users.noreply.github.com>
2024-06-04 19:53:19 +0000

    testing for grade

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,21 +1,42 @@
-class DiceController <ApplicationController
-
+class DiceController < ApplicationController
   def home
-    render({:template => "dice_template/home"})
+    render({ :template => "dice_template/home" })
   end
 
   def two_six_sided
-    render({:template => "dice_template/two"})
+    @rolls = []
+
+    2.times do
+      die = rand(1..6)
+      @rolls.push(die)
+    end
+    render({ :template => "dice_template/two" })
   end
+
   def two_ten_sided
-    render({:template => "dice_template/ten"})
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+      @rolls.push(die)
+    end
+    render({ :template => "dice_template/ten" })
   end
+
   def one_twenty_sided
-    render({:template => "dice_template/twenty"})
+    
+      @rolls = rand(1..20)
+  
+    render({ :template => "dice_template/twenty" })
   end
+
   def five_four_sided
-    render({:template => "dice_template/four"})
+    @rolls = []
 
+    5.times do
+      die = rand(1..4)
+      @rolls.push(die)
+    end
+    render({ :template => "dice_template/four" })
   end
-
 end
